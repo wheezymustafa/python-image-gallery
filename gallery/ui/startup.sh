af06c954d442a0a598729cb58d523710867a0bc1
EC2 Default User <ec2-user@ip-10-0-1-249.us-east-2.compute.internal>
2020-06-18 03:28:41 +0000

    fixing structure

--- gallery/ui/startup.sh
+++ gallery/ui/startup.sh
@@ -1,3 +1,3 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
-flask run --host=0.0.0.0
+flask run --host=0.0.0.0 --port 5555
---
+++ gallery/ui/user_admin.py
@@ -0,0 +1,123 @@
+import psycopg2
+from secrets_client import get_secret
+import json
+
+dbName = "users"
+dbUser = "image_gallery"
+#host = "imagegallery.ctvpfstspksz.us-east-2.rds.amazonaws.com"
+host = "image-gallery-priv.ctvpfstspksz.us-east-2.rds.amazonaws.com"
+port = "5432"
+connection = None
+menu = """
+Image Gallery User Administration v0.1
+==============================
+[L]ist Users
+[A]dd User
+[E]dit User
+[D]elete User
+[Q]uit
+    
+Enter a choice: """
+
+
+def get_image_gallery_secret():
+    return get_secret()
+
+
+def get_password():
+    print('Retrieving secret..')
+    secret = get_image_gallery_secret()
+    secret_dict = json.loads(secret)
+    print('Retrieved secret..')
+    return secret_dict['password']
+
+
+def connect():
+    print('Connecting to {host}..'.format(host=host))
+    password = get_password()
+    global connection
+    connection = psycopg2.connect(database=dbName,
+                                  user=dbUser,
+                                  password=password,
+                                  host=host,
+                                  port=port)
+    connection.set_session(autocommit=True)
+    print('Connected to {host}'.format(host=host))
+
+
+def get_cursor():
+    if not connection:
+        connect()
+    return connection.cursor()
+
+
+def execute(query, args=None):
+    cur = get_cursor()
+    if not args:
+        cur.execute(query)
+    else:
+        cur.execute(query, args)
+    return cur
+
+
+def add_user_menu():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    fullname = input("Enter full name: ")
+    add_user(username, password, fullname)
+
+def edit_user_menu():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    fullname = input("Enter full name: ")
+    update_user(username, password, fullname)
+
+def delete_user_menu():
+    delete_user(input("Enter username: "))
+    print('Deleted user')
+
+def list_users_menu():
+    result = get_all_users()
+    print("\nusername, password, fullname")
+    for row in result:
+        print(row)
+
+
+
+def add_user(username, password, fullname):
+    cur = get_cursor()
+    query = "insert into users (username, password, fullname) values (%s, %s, %s);"
+    execute(query, (username, password, fullname))
+
+def update_user(username, password, fullname):
+    query = "update users set password = %s, fullname = %s where username = %s;"
+    execute(query, (password, fullname, username))
+
+def delete_user(username):
+    query = "delete from users where username = %s;"
+    execute(query, (username,))
+
+def get_all_users():
+    query = "select * from users;"
+    return execute(query)
+
+
+def main():
+    connect()
+    choice = input(menu)
+    while choice not in ['q', 'Q']:
+        if choice in ['l', 'L']:
+            list_users_menu()
+        elif choice in ['a', 'A']:
+            add_user_menu()
+        elif choice in ['e', 'E']:
+            edit_user_menu()
+        elif choice in ['d', 'D']:
+            delete_user_menu()
+        else:
+            print('Invalid option.')
+        choice = input(menu)
+    print("Okay, Bye.")
+
+if __name__ == '__main__':
+    main()
