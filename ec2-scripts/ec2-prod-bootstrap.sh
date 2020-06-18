13add4e07e2f4ed50dcf4f8db09bc1dbb54564fd
EC2 Default User <ec2-user@ip-10-0-1-249.us-east-2.compute.internal>
2020-06-18 17:08:38 +0000

    removing ec2 scripts from project repo

--- ec2-scripts/ec2-prod-bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-export IMAGE_GALLERY_BOOSTRAP_VERSION="1.0"
-export SCRIPT_NAME="ec2-prod-latest.sh"
-aws s3 cp s3://edu.au.cc.dam0045.image-gallery-config/$SCRIPT_NAME ./
-/usr/bin/bash $SCRIPT_NAME
