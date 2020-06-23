5d6d9da6a5cdb231d612fdea919fa562816aa59c
wheezymustafa <danielm8686@gmail.com>
2020-06-22 20:31:08 -0500

    Update admin_service.py

--- gallery/ui/admin_service.py
+++ gallery/ui/admin_service.py
@@ -1,20 +1,20 @@
-from . import user_admin
+from . import user_dao
 
-user_admin.connect()
+user_dao.connect()
 
 def get_users():
     users = []
-    res = user_admin.get_all_users()
+    res = user_dao.get_all_users()
     for user in res:
         users.append({'username': user[0], 'fullname': user[2]})
     return users
 
 def add_user(username, password, fullname):
-    user_admin.add_user(username, password, fullname)
+    user_dao.add_user(username, password, fullname)
 
 def update_user(username, password, fullname):
-    user_admin.update_user(username, password, fullname)
+    user_dao.update_user(username, password, fullname)
 
 def delete_user(username):
-    user_admin.delete_user(username)
+    user_dao.delete_user(username)
    
