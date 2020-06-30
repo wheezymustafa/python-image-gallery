1d4ae830bb56ce608421a37579819634621eccd5
EC2 Default User <ec2-user@ip-10-0-1-87.us-east-2.compute.internal>
2020-06-30 04:41:01 +0000

    adding image svc

--- gallery/ui/image_service.py
+++ gallery/ui/image_service.py
@@ -3,7 +3,7 @@ import os
 
 client = boto3.client('s3')
 IMAGE_BUCKET_NAME='edu.au.cc.dam0045.image-gallery'
-IMAGE_PATH='/images'
+IMAGE_PATH='images'
 
 def get_image(imageid):
     response = client.get_object(Bucket=IMAGE_BUCKET_NAME, Key=imageid)
@@ -11,11 +11,16 @@ def get_image(imageid):
     if response is None:
         return None
     else:
-        save_image(imageid, response.data)
-
+        file_path = save_image(imageid, response['Body'].read())
+        return file_path
 
 def save_image(imageid, image):
     if not os.path.exists(IMAGE_PATH):
         os.makedirs(IMAGE_PATH)
 
-    
+    file_path = '{}/{}'.format(IMAGE_PATH, imageid)
+    file = open(file_path, 'wb')
+    file.write(image)
+    file.close()
+
+    return file_path
