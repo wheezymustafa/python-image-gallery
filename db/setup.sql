aa33f51f5c5983352e00e8ce3152f157becb3deb
wheezymustafa <danielm8686@gmail.com>
2020-07-01 23:49:54 -0500

    Update setup.sql

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,4 @@
 create table if not exists users(username varchar(50) primary key, password varchar(50), fullname varchar(50));
 create table if not exists userimages(username varchar(50) references users(username), imageid varchar(50), primary key(username, imageid));
-create user image_gallery with password '1mage_ga113ry!';
 grant all on all tables in schema public to image_gallery;
 insert into users (username, password, fullname) values ('dam0045', '111', 'daniel mustafa'), ('dongji', 'cpsc4973', 'dongji the GTA');
